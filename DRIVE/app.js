a118e330ccf0ae4cd06012a9e189e166b1ac04cb
cn-gourav <mrgusain558@gmail.com>
2025-05-07 23:49:53 +0530

    ADD :: NPM validator

--- DRIVE/app.js
+++ DRIVE/app.js
@@ -4,7 +4,8 @@ const userRouter = require('./routes/user.routes');
 
 
 app.set('view engine','ejs')
-
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use('/user' ,userRouter);
 
 app.listen(3000,()=>{
--- DRIVE/routes/user.routes.js
+++ DRIVE/routes/user.routes.js
@@ -1,11 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { body, vaildationResult, validationResult } = require("express-validator");
 
+router.get("/register", (req, res) => {
+  res.render("register");
+});
 
-router.get('/register',(req,res)=>{
-    res.render('register');
-})
+router.post(
+  '/register',
+  body("email").trim().isEmail(),
+  body("password").trim().isLength({ min: 5 }),
+  body("username").trim().isLength({ min: 3 }),
+  (req, res) => {
 
+    const errors = validationResult(req);
+    
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array(),
+            message: 'Invaild data'
+        })
+    }
+ 
+    // console.log(errors)
+    res.send(errors)
 
+    // console.log(req.body);
+    // res.send("success");
+  }
+);
 
 module.exports = router;
