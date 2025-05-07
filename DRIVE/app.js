b54f41cad86b6fcbf1a229c19c9b0e99e57a1168
cn-gourav <mrgusain558@gmail.com>
2025-05-07 23:23:30 +0530

    Using tailwind And Flowbite created for login page

--- DRIVE/app.js
+++ DRIVE/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const userRouter = require('./routes/user.routes');
 
-app.get('/',(req,res)=>{
-    res.send("hwllo world")
-})
+
+app.set('view engine','ejs')
+
+app.use('/user' ,userRouter);
 
 app.listen(3000,()=>{
     console.log("server listening in 3000")
